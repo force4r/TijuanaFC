5903d34805ce09bd758f69f6c1ceb98c65fc8482
MatiasRodriguez <95397306+force4r@users.noreply.github.com>
2022-10-17 15:10:36 -0300

    modificaciones varias

--- TijuanaFC/script.js
+++ TijuanaFC/script.js
@@ -4,3 +4,10 @@ const navbar = document.getElementsByClassName("navbar")[0]
 toggleButton.addEventListener("click", () =>{
     navbar.classList.toggle("active")
 })
+
+const ctgria = document.getElementsByClassName("ctgria")[0]
+
+ctgria.addEventListener("click", () => {
+
+    ctgria.classList.toggle("active")
+})
