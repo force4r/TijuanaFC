087f7c260431aa2f10d105b8d1c4b35f87a052b1
MatiasRodriguez <95397306+force4r@users.noreply.github.com>
2022-10-29 17:23:00 -0300

    menu hamburguesa

--- TijuanaFC/script.js
+++ TijuanaFC/script.js
@@ -3,6 +3,9 @@ const navbar = document.getElementsByClassName("navbar")[0]
 
 toggleButton.addEventListener("click", () =>{
     navbar.classList.toggle("active")
+    
+    
+
 })
 
 
@@ -15,4 +18,17 @@ function Close(){
 function Open(reference){
     fullscreenBox.style.display = "flex";
     full.src = reference;
+}
+
+
+document.querySelector(".toggle-button").addEventListener("click", animarbarras)
+
+var barra1=document.querySelector(".bar1");
+var barra2=document.querySelector(".bar2");
+var barra3=document.querySelector(".bar3");
+
+function animarbarras(){
+    barra1.classList.toggle("activebar1");
+    barra2.classList.toggle("activebar2");
+    barra3.classList.toggle("activebar3");
 }
