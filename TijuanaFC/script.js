bed0cea05349565bcece95fb1a37741d4548077c
MatiasRodriguez <95397306+force4r@users.noreply.github.com>
2022-10-29 14:20:22 -0300

    se mejoro galeria de imagenes y se modificaron algunas cosas mas.

--- TijuanaFC/script.js
+++ TijuanaFC/script.js
@@ -4,3 +4,15 @@ const navbar = document.getElementsByClassName("navbar")[0]
 toggleButton.addEventListener("click", () =>{
     navbar.classList.toggle("active")
 })
+
+
+const fullscreenBox = document.getElementById("fullscreenBox"), full = document.getElementById("full");
+
+function Close(){
+    fullscreenBox.style.display = "none";
+}
+
+function Open(reference){
+    fullscreenBox.style.display = "flex";
+    full.src = reference;
+}
